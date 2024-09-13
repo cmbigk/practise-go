69fcc8aae5ea00052c7138dd9c7996751d798032
chmyint <bigk89865@gmail.com>
2024-09-13 15:15:18 +0300

    boaungdin

--- firstword.go
+++ firstword.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
-	"piscine"
 	"strings"
 )
 
 func FirstWord(s string) string {
 	checkSpace := strings.TrimSpace(s)
+	if checkSpace == "" {
+		return "" + "\n"
+
+	}
 
 	result := strings.Split(checkSpace, " ")
 	return result[0]
 }
 
 func main() {
-	fmt.Print(piscine.FirstWord("hello there"))
-	fmt.Print(piscine.FirstWord(""))
-	fmt.Print(piscine.FirstWord("hello   .........  bye"))
+	fmt.Print(FirstWord("hello there"))
+	fmt.Print(FirstWord(""))
+	fmt.Print(FirstWord("hello   .........  bye"))
 }
---
+++ repeatalpha.go
@@ -0,0 +1,5 @@
+package main
+
+func RepeatAlpha(s string) string {
+
+}
