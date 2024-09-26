d0ae0890033724e5d5cc520fc501835842ca4adf
chmyint <bigk89865@gmail.com>
2024-09-26 12:19:46 +0300

    now, I get revcomb!

---
+++ printrevcomb/printrevcomb.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func PrintRevComb() {
+	result := ""
+	for c1 := '9'; c1 >= '0'; c1-- {
+		for c2 := c1 - 1; c2 >= '0'; c2-- {
+			for c3 := c2 - 1; c3 >= '0'; c3-- {
+				if result != "" {
+					result += ", "
+				}
+				result += fmt.Sprintf("%c%c%c", c1, c2, c3)
+			}
+		}
+	}
+}
+
+func main() {
+	PrintRevComb()
+}
