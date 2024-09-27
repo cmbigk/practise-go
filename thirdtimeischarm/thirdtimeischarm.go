03db6557754e49ba574f143357d43d4f54d0245a
chmyint <bigk89865@gmail.com>
2024-09-27 15:34:31 +0300

    I know it now! I can do it by myself!

--- thirdtimeischarm/thirdtimeischarm.go
+++ thirdtimeischarm/thirdtimeischarm.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func ThirdTimeIsACharm(str string) string {
-	if len(str) == 0 {
-		return "" + "\n"
-	}
 	if len(str) < 3 {
-		return "\n"
+		return "" + "\n"
+
 	}
-	result := ""
-	for i := 2; i < len(str); i = i + 3 {
+	var result string
+	for i := 2; i < len(str); i += 3 {
 		result += string(str[i])
 	}
 	return result + "\n"
 }
+
 func main() {
 	table := []string{"1234556789", "QKplq%QSw", "", "Kimetsu no Yaiba", "Z", "email123@live.fr", "8595485-52", "-552", "abc", "w58tw7474abc", "fifa world cup `2022`", "a b c d e f"}
 
