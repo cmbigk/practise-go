518d4ddca5cbb8264ca9ba676d01f0df61ec471c
chmyint <bigk89865@gmail.com>
2024-09-27 11:18:07 +0300

    now, i know fromto

--- fromto/fromto.go
+++ fromto/fromto.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func FromTo(from int, to int) string {
-	if 
+	if from > 99 || to > 99 || from < 0 || to < 0 {
+		return "Invalid" + "\n"
+	}
+	result := ""
+	if from < to {
+		for i := from; i <= to; i++ {
+			if i < 10 {
+				result += "0" + strconv.Itoa(i)
+			} else {
+				result += strconv.Itoa(i)
+			}
+			if i != to {
+				result += ", "
+			}
+		}
+	}
+	if from > to {
+		for i := from; i >= to; i-- {
+			if i < 10 {
+				result += "0" + strconv.Itoa(i)
+			} else {
+				result += strconv.Itoa(i)
+			}
+			if i != to {
+				result += ", "
+			}
+		}
+	}
+
+	if from == to {
+		if from < 10 {
+			result += "0" + strconv.Itoa(from)
+		} else {
+			result += strconv.Itoa(from)
+		}
+	}
+	return result + "\n"
+}
+
+func main() {
+	fmt.Print(FromTo(1, 10))
+	fmt.Print(FromTo(10, 1))
+	fmt.Print(FromTo(10, 10))
+	fmt.Print(FromTo(100, 10))
 }
