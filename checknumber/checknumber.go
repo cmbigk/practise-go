38016a3d106d6cdf42a4dc137c0a5c5b354c71ea
chmyint <bigk89865@gmail.com>
2024-09-30 10:13:10 +0300

    layy kyint

--- checknumber/checknumber.go
+++ checknumber/checknumber.go
@@ -1,4 +1,6 @@
-package piscine
+package main
+
+import "fmt"
 
 func checknumber(k string) bool {
 	for _, c := range k {
@@ -7,4 +9,21 @@ func checknumber(k string) bool {
 		}
 	}
 	return false
+}
+
+func main() {
+	args := []string{
+		"123",
+		"H1ll0",
+		"",
+		"1",
+		"1.1",
+		"Containe1number",
+		"     ",
+		"upson lorem ipsum",
+	}
+
+	for _, arg := range args {
+		fmt.Printf("checknumber(%q) = %t\n", arg, checknumber(arg))
+	}
 }
