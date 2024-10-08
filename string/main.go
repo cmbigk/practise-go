6addd767b535e4178f8f6db46528ea521fee56aa
cmbigk <bigk89865@gmail.com>
2024-10-09 02:43:06 +0300

    learning

--- string/main.go
+++ string/main.go
@@ -20,8 +20,14 @@ func lrnStrLower() string {
 	return strings.ToLower(str)
 }
 
+func lrnStrTrim() string {
+	str := "NHello,how are you?NNNN" //to remove the specific char from the start or end ofstr, we can use TrimSpace to remove the unwanted spaces
+	return strings.Trim(str, "N")
+}
+
 func main() {
 	fmt.Printf("%v\n", learningStrContain())
 	fmt.Printf("%v\n", lrnStrHassuffix())
 	fmt.Printf("%v\n", lrnStrLower())
+	fmt.Printf("%v\n", lrnStrTrim())
 }
