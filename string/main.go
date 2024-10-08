f7fc82129394f1a82bbe4edbf2ecd1bb69ef6c16
cmbigk <bigk89865@gmail.com>
2024-10-09 02:34:23 +0300

    learning

--- string/main.go
+++ string/main.go
@@ -12,10 +12,16 @@ func learningStrContain() bool { //to check if something is in a string
 
 func lrnStrHassuffix() bool {
 	str := "Hello, how are you?" //to check if a string has a suffix, also we can use HasPrefix to check the prefix
-	return strings.HasSuffix(str, "Hello")
+	return strings.HasSuffix(str, "you?")
+}
+
+func lrnStrLower() string {
+	str := "HELLO, HOW ARE YOU?" //to convert a string to lowercase, we can use ToUpper to convert to uppercase
+	return strings.ToLower(str)
 }
 
 func main() {
 	fmt.Printf("%v\n", learningStrContain())
 	fmt.Printf("%v\n", lrnStrHassuffix())
+	fmt.Printf("%v\n", lrnStrLower())
 }
