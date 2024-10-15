9399801e974b1d65acf5157de775acb0566f2417
cmbigk <bigk89865@gmail.com>
2024-10-15 23:57:54 +0300

    now I understand!

---
+++ concatslice/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func ConcatSlice(slice1, slice2 []int) []int {
+	var result []int
+	// Concatenate the two slices
+	result = append(result, slice1...)
+	result = append(result, slice2...)
+	// Return the concatenated slice
+	return result
+
+}
+
+func main() {
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
+}
