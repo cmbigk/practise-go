a84a953152f17ef0b2bdd254fa9924b08c94707b
cmbigk <bigk89865@gmail.com>
2024-10-16 00:53:08 +0300

    I still need to work on this!

--- concatalternate/main.go
+++ concatalternate/main.go
@@ -4,35 +4,29 @@ import "fmt"
 
 func ConcatAlternate(slice1, slice2 []int) []int {
 	var result []int
-	len1 := len(slice1)
-	len2 := len(slice2)
 
-	if len1 >= len2 {
-		for i := 0; i < len1; i++ {
-			if i < len1 {
-				result = append(result, slice1[i])
-			}
-			if i < len2 {
-				result = append(result, slice2[i])
-			}
-		}
-	} else {
-		for i := 0; i < len2; i++ {
-			if i < len1 {
-				result = append(result, slice1[i])
-			}
-			if i < len2 {
-				result = append(result, slice2[i])
-			}
+	if len(slice1) < len(slice2) {
+		slice1, slice2 = slice2, slice1
+	}
+	for i, v := range slice1 {
+		result = append(result, v)
+		if i < len(slice2) {
+			result = append(result, slice2[i])
 		}
 	}
 	return result
-
 }
 
 func main() {
-	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
-	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
-	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
+	args := [][][]int{
+		{{1, 2, 3}, {4, 5, 6}},
+		{{1, 2, 3}, {4, 5}},
+		{{}, {4, 5, 6}},
+		{{1, 2, 3}, {}},
+		{{}, {}},
+		{{1, 2, 4}, {10, 20, 30, 40, 50}},
+	}
+	for _, arg := range args {
+		fmt.Println("ConcatAlternate", ConcatAlternate(arg[0], arg[1]))
+	}
 }
