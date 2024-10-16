a1eb0c59c9f66e376deb6fe5a2392646590a58b4
cmbigk <bigk89865@gmail.com>
2024-10-17 00:18:33 +0300

    ok I got it!

---
+++ canjump/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func CanJump(steps []uint) bool {
+	if len(steps) == 0 {
+		return false
+	}
+
+	maxjump := 0
+	for i := 0; i < len(steps); i++ {
+		if i > maxjump {
+			return false
+		}
+
+		maxjump = max(maxjump, i+int(steps[i]))
+		if maxjump == len(steps)-1 || len(steps) == 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	input1 := []uint{2, 3, 1, 1, 4}
+	fmt.Println(CanJump(input1))
+
+	input2 := []uint{3, 2, 1, 0, 4}
+	fmt.Println(CanJump(input2))
+
+	input3 := []uint{0}
+	fmt.Println(CanJump(input3))
+
+	input4 := []uint{0, 1, 2, 3, 4, 1, 1, 0}
+	fmt.Println(CanJump(input4))
+}
