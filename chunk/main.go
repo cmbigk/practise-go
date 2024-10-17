55d55fa3a3492f6acb152efe293b7a1dc94fb588
cmbigk <bigk89865@gmail.com>
2024-10-17 11:18:57 +0300

    ok!!

--- chunk/main.go
+++ chunk/main.go
@@ -2,14 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
-	Chunk([]int{}, 10)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
-}
-
 func Chunk(slice []int, size int) {
 
 	if size <= 0 {
@@ -26,4 +18,12 @@ func Chunk(slice []int, size int) {
 		result = append(result, slice[i:end])
 	}
 	fmt.Println(result)
+}
+
+func main() {
+	Chunk([]int{}, 10)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
 }
