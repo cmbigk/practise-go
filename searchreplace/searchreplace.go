03d4dfde62242f5f8cd8fade8c7d0f739f22bef2
cmbigk <bigk89865@gmail.com>
2024-10-20 21:01:34 +0300

    Om Namo Narayana

--- searchreplace/searchreplace.go
+++ searchreplace/searchreplace.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	if len(os.Args) != 4 {
 		return
 	}
+
 	str := os.Args[1]
 	old := os.Args[2]
 	new := os.Args[3]
 
-	// Ensure 'old' and 'new' are single characters
-	if len(old) != 1 || len(new) != 1 {
+	if len(old) != 1 && len(new) != 1 {
 		return
 	}
 
-	// Replace occurrences of 'old' with 'new'
 	result := ""
 	for _, char := range str {
-		if string(char) == old { // Compare current character with 'old'
-			result += new // Append 'new' if it matches
+		if string(char) == old {
+			result += new
 		} else {
-			result += string(char) // Append original character if it doesn't match
+			result += string(char)
 		}
 	}
-
-	fmt.Println(result)
+	for _, c := range result {
+		z01.PrintRune(c)
+	}
 }
