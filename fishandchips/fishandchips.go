753b93274e2823e22096634272ef2984154dfe3b
cmbigk <bigk89865@gmail.com>
2024-10-20 10:54:06 +0300

    pkk

--- fishandchips/fishandchips.go
+++ fishandchips/fishandchips.go
@@ -1,17 +1,30 @@
 package main
 
+import "fmt"
+
 func FishAndChips(n int) string {
 	if n < 0 {
 		return "error: number is negative"
 	}
-	if (n%2 == 0) && (n%3 == 0) {
-		return "Fish and Chips"
+
+	if n%2 == 0 && n%3 == 0 {
+		return "fish and chips"
 	}
+
 	if n%2 == 0 {
-		return "Fish"
+		return "fish"
 	}
+
 	if n%3 == 0 {
-		return "Chips"
+		return "chips"
+	} else {
+		return "error: non divisible"
 	}
-	return "error: non divisible"
+
+}
+
+func main() {
+	fmt.Println(FishAndChips(15))
+	fmt.Println(FishAndChips(7))
+	fmt.Println(FishAndChips(-5))
 }
