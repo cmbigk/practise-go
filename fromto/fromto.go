0e6ed9686a8380d3e7243b1b84fd7e357b7205d8
cmbigk <bigk89865@gmail.com>
2024-10-24 22:36:26 +0300

    practising

--- fromto/fromto.go
+++ fromto/fromto.go
@@ -5,17 +5,25 @@ import (
 	"strconv"
 )
 
+func main() {
+	fmt.Print(FromTo(1, 10))
+	fmt.Print(FromTo(10, 1))
+	fmt.Print(FromTo(10, 10))
+	fmt.Print(FromTo(100, 10))
+}
+
 func FromTo(from int, to int) string {
-	if from < 0 || from > 99 || to < 0 || to > 99 {
-		return "Invalid" + "\n"
-	}
 	result := ""
+	if from < 0 || to < 0 || from > 99 || to > 99 {
+		return "Invalid"
+	}
 	if from == to {
 		if from < 10 {
 			result += "0" + strconv.Itoa(from)
 		} else {
 			result += strconv.Itoa(from)
 		}
+
 	}
 
 	if from < to {
@@ -30,7 +38,6 @@ func FromTo(from int, to int) string {
 			}
 		}
 	}
-
 	if from > to {
 		for i := from; i >= to; i-- {
 			if i < 10 {
@@ -41,16 +48,9 @@ func FromTo(from int, to int) string {
 			if i != to {
 				result += ", "
 			}
+
 		}
 
 	}
-
 	return result + "\n"
-}
-
-func main() {
-	fmt.Print(FromTo(1, 10))
-	fmt.Print(FromTo(10, 1))
-	fmt.Print(FromTo(10, 10))
-	fmt.Print(FromTo(100, 10))
 }
