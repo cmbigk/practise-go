a1f61f59405ed1d137e9aacf18f4be031e6a3625
cmbigk <bigk89865@gmail.com>
2024-10-24 13:29:58 +0300

    pk

---
+++ punctuation/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func PunPun(pun []string) []string {
+
+	// Handle punctuation at the start of the first word (index 0)
+	for len(pun[0]) > 0 && strings.Contains(".,!?;:", string(pun[0][0])) {
+		// If the first character of the first word is punctuation, remove it
+		pun[0] = pun[i][1:]
+	}
+
+	// Process all other words starting from index 1
+	for i := 1; i < len(pun); i++ {
+		// If the current word starts with punctuation
+		for len(pun[i]) > 0 && strings.Contains(".,!?;:", string(pun[i][0])) {
+			// Attach punctuation to the previous word
+			pun[i-1] = pun[i-1] + string(pun[i][0])
+			// Remove the punctuation from the current word
+			pun[i] = pun[i][1:]
+		}
+	}
+
+	return removeTheSlice(pun)
+}
+
+func main() {
+	input := []string{"Hello", ",", "world", "!", "How", "are", "you", "?"}
+	fmt.Println("Original:", input)
+
+	output := PunPun(input)
+	fmt.Println("Processed:", output)
+
+	testCases := [][]string{
+		{"Hello", ",", "world", "!"},
+		{"!!!", "How", "are", "you?", "?"},
+		{".....", ".", ".", "Test"},
+		{"NoPunctuationsHere"},
+	}
+
+	for _, testCase := range testCases {
+		fmt.Println("Testing:", testCase)
+		result := PunPun(testCase)
+		fmt.Println("Result:", result)
+	}
+}
