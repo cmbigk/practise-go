fccc207f03c866d43ce6b107a509f3f4f4271a86
cmbigk <bigk89865@gmail.com>
2024-10-25 20:15:57 +0300

    okkk

--- itoa/itoa.go
+++ itoa/itoa.go
@@ -1,36 +1,35 @@
 package main
 
-import "fmt"
-
-func Itoa(n int) string {
-	if n == 0 {
-		return "0"
-	}
-
-	isNegative := false
-	if n < 0 {
-		isNegative = true
-		n = -n
-	}
-
-	result := ""
-	for n > 0 {
-		addingLastDigit := n % 10
-		result = string(addingLastDigit+'0') + result
-		n = n / 10
-	}
-
-	// Add the negative sign if needed
-	if isNegative {
-		result = string('-') + result
-	}
-
-	return result
-}
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println(Itoa(12345))
 	fmt.Println(Itoa(0))
 	fmt.Println(Itoa(-1234))
 	fmt.Println(Itoa(987654321))
+}
+
+func Itoa(n int) string {
+	if n == 0 {
+		return "0"
+	}
+
+	result := ""
+	isNegative := n < 0
+	if isNegative {
+		n = -n
+	}
+
+	for n > 0 {
+		addingLastDigit := n % 10
+		result = string(addingLastDigit+'0') + result
+		n = n / 10
+	}
+
+	if isNegative {
+		result = string('-') + result
+	}
+	return result
 }
