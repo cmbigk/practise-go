705beec33232b2e9fd721f6fd3bd82929ac59bfb
cmbigk <bigk89865@gmail.com>
2024-10-25 11:36:08 +0300

    okk

--- thirdtimeischarm/thirdtimeischarm.go
+++ thirdtimeischarm/thirdtimeischarm.go
@@ -1,23 +1,23 @@
 package main
 
-import "fmt"
-
-func ThirdTimeIsACharm(str string) string {
-	if len(str) < 3 {
-		return "" + "\n"
-
-	}
-	result := ""
-	for i := 2; i < len(str); i += 3 { //starts at index 2 bcuz we want to start from third character of the string, then just add 3 in that!!
-		result += string(str[i])
-	}
-	return result
-}
+import (
+	"fmt"
+)
 
 func main() {
-	table := []string{"1234556789", "QKplq%QSw", "", "Kimetsu no Yaiba", "Z", "email123@live.fr", "8595485-52", "-552", "abc", "w58tw7474abc", "fifa world cup `2022`", "a b c d e f"}
+	fmt.Print(ThirdTimeIsACharm("123456789"))
+	fmt.Print(ThirdTimeIsACharm(""))
+	fmt.Print(ThirdTimeIsACharm("a b c d e f"))
+	fmt.Print(ThirdTimeIsACharm("12"))
+}
 
-	for _, s := range table {
-		fmt.Println(ThirdTimeIsACharm(s))
+func ThirdTimeIsACharm(str string) string {
+	result := ""
+	if len(str) < 3 {
+		return "" + "\n"
 	}
+	for i := 2; i < len(str); i += 3 {
+		result += string(str[i])
+	}
+	return result + "\n"
 }
