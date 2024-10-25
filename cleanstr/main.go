dffafbc3d134866fc7f9434345fbd098aa01299e
cmbigk <bigk89865@gmail.com>
2024-10-25 19:13:17 +0300

    ok

--- cleanstr/main.go
+++ cleanstr/main.go
@@ -8,23 +8,20 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		z01.PrintRune('\n')
 		return
 	}
 
 	str := os.Args[1]
-	wordInBetween := false
-
-	for i, char := range str {
-		if char == ' ' || char == '\t' {
-			if wordInBetween && i != 0 && str[i-1] != ' ' && str[i-1] != '\t' {
+	wordsInBetween := false
+	for i, c := range str {
+		if c == ' ' || c == '\t' {
+			if wordsInBetween && i != 0 && str[i-1] != ' ' && str[i-1] != '\t' {
 				z01.PrintRune(' ')
 			}
 		} else {
-			z01.PrintRune(char)
-			wordInBetween = true
+			z01.PrintRune(c)
+			wordsInBetween = true
 		}
-
 	}
 	z01.PrintRune('\n')
 }
