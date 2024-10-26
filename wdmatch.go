69f7ee1f063c0fddb9160b9ef46d420a3a0684d6
cmbigk <bigk89865@gmail.com>
2024-10-26 15:06:53 +0300

    ok, I understand wdmatch now!

---
+++ wdmatch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func isInStr(str1, str2 string) bool {
+	i := 0
+	j := 0
+	for i < len(str1) && j < len(str2) {
+		if str1[i] == str2[j] {
+			i++
+		}
+		j++
+
+	}
+	return i == len(str1)
+}
+
+func main() {
+	str1 := os.Args[1]
+	str2 := os.Args[2]
+
+	if isInStr(str1, str2) {
+		for _, char := range str1 {
+			z01.PrintRune(char)
+		}
+
+	}
+	z01.PrintRune('\n')
+}
