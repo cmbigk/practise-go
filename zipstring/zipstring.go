3f4d3151ca99b99e36d24c02e7b5006d7bf8016f
cmbigk <bigk89865@gmail.com>
2024-10-26 11:04:38 +0300

    first commit of the day!

--- zipstring/zipstring.go
+++ zipstring/zipstring.go
@@ -5,13 +5,19 @@ import (
 	"strconv"
 )
 
+func main() {
+	fmt.Println(ZipString("YouuungFellllas"))
+	fmt.Println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
+	fmt.Println(ZipString("Helloo Therre!"))
+}
+
 func ZipString(s string) string {
 	result := ""
 	i := 0
 
 	for i < len(s) {
 		count := 1
-		for i+1 < len(s) && s[i] == s[i+1] {
+		for i+1 < len(s) && s[i+1] == s[i] {
 			count++
 			i++
 		}
@@ -19,18 +25,4 @@ func ZipString(s string) string {
 		i++
 	}
 	return result
-}
-
-func main() {
-	args := []string{
-		"aaaa",
-		"zzzzzZZZZZZ",
-		"",
-		"ziiiiipMeee",
-		"hhellloTthereYouuunggFelllas",
-	}
-
-	for _, arg := range args {
-		fmt.Println(ZipString(arg))
-	}
 }
