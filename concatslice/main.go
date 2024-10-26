7ffe4f8e59b9e4c8ae3c3a01cb1379771e4e2f07
cmbigk <bigk89865@gmail.com>
2024-10-26 16:41:09 +0300

    okkk,

--- concatslice/main.go
+++ concatslice/main.go
@@ -1,34 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
+}
 
 func ConcatSlice(slice1, slice2 []int) []int {
 	var result []int
-	for _, v := range slice1 {
-		result = append(result, v)
+
+	for _, c := range slice1 {
+		result = append(result, c)
 	}
 
-	for _, v := range slice2 {
-		result = append(result, v)
+	for _, d := range slice2 {
+		result = append(result, d)
 	}
 
-	//
 	return result
 
-}
-
-func main() {
-	args := [][][]int{
-		{{1, 2, 3}, {4, 5, 6}},
-		{{}, {-10, 0, 2}},
-		{{-10, 0, 2}, {}},
-		{{}, {}},
-		{{1, 2, 3}, {4, 5, 6, 3, 4, 5, 6}},
-		{{0, 0, 0}, {0, 0, 0}},
-		{{1, 2, 3}, {4, 5, 6}},
-	}
-
-	for _, arg := range args {
-		fmt.Println("ConcatSlice", ConcatSlice(arg[0], arg[1]))
-	}
 }
