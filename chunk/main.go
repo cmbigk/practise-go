93bd5d1c3d044578c284731a24cbebe3b50be5f8
cmbigk <bigk89865@gmail.com>
2024-10-27 18:08:35 +0200

    okk, got it

--- chunk/main.go
+++ chunk/main.go
@@ -16,15 +16,15 @@ func Chunk(slice []int, size int) {
 		return
 	}
 
-	chunks := [][]int{}
+	var result [][]int
 	for i := 0; i < len(slice); i += size {
 		end := i + size
 
 		if end > len(slice) {
 			end = len(slice)
 		}
-		chunks = append(chunks, slice[i:end])
-	}
-	fmt.Println(chunks)
 
+		result = append(result, slice[i:end])
+	}
+	fmt.Println(result)
 }
