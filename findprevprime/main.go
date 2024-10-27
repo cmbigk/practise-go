0a0cb8bccef1e5334acf96c61d23a2b1728a38c9
cmbigk <bigk89865@gmail.com>
2024-10-27 15:43:02 +0200

    okk

--- findprevprime/main.go
+++ findprevprime/main.go
@@ -7,15 +7,15 @@ func main() {
 	fmt.Println(FindPrevPrime(4))
 }
 
-func isPrime(nb int) bool {
-	if nb < 2 {
+func isPrime(n int) bool {
+	if n < 2 {
 		return false
 	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
+
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
 			return false
 		}
-
 	}
 	return true
 }
@@ -25,6 +25,7 @@ func FindPrevPrime(nb int) int {
 		if isPrime(i) {
 			return i
 		}
+
 	}
 	return 0
 
