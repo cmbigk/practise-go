3282869c90a42a3fa989035dcd3f61cb62c2fc96
cmbigk <bigk89865@gmail.com>
2024-10-27 22:23:35 +0200

    okk, thanks to Milli

--- printmemory/main.go
+++ printmemory/main.go
@@ -1,20 +1,16 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-
 	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
 }
 
 func hex2Rune(s byte) rune {
 	if s < 10 {
 		return rune(s + '0')
-	} else {
-		return rune(s - 10 + 'a')
 	}
+	return rune(s - 10 + 'a')
 }
 
 func PrintMemory(arr [10]byte) {
@@ -24,20 +20,21 @@ func PrintMemory(arr [10]byte) {
 
 		z01.PrintRune(hex2Rune(firstdigit))
 		z01.PrintRune(hex2Rune(seconddigit))
-
 		z01.PrintRune(' ')
 
-		if i%4 == 3 {
+		if (i+1)%4 == 0 {
 			z01.PrintRune('\n')
 		}
 	}
 	z01.PrintRune('\n')
-	for _, c := range arr {
-		if c >= 32 && c <= 126 {
-			z01.PrintRune(rune(c))
+	for _, char := range arr {
+		if char >= 32 && char <= 126 {
+			z01.PrintRune(rune(char))
 		} else {
-			z01.PrintRune(rune('.'))
-		}
-	}
+			z01.PrintRune('.')
 
+		}
+
+	}
+	z01.PrintRune('\n')
 }
