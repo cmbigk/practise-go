c5df74dd98aac45c95fc27910ada860ba00bc0b2
cmbigk <bigk89865@gmail.com>
2024-10-27 11:37:56 +0200

    okk, still need to do it

--- reversestrcap/main.go
+++ reversestrcap/main.go
@@ -6,26 +6,22 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func IsUpper(r rune) bool {
-	if r >= 'A' && r <= 'Z' {
-		return true
-	}
-	return false
+func isLower(c rune) bool {
+	return c >= 'a' && c <= 'z'
 }
 
-func IsLower(r rune) bool {
-	if r >= 'a' && r <= 'z' {
-		return true
-	}
-	return false
+func isUpper(c rune) bool {
+	return c >= 'A' && c <= 'Z'
 }
 
-func ReverseStrCap(s string) string {
+func reverStrCap(s string) string {
 	result := []rune(s)
 	for i := 0; i < len(result); i++ {
-		if (i == len(result)-1 || result[i+1] == ' ') && IsLower(result[i]) {
-			result[i] -= 32
-		} else if IsUpper(result[i]) {
+		if i == len(result)-1 || result[i+1] == ' ' {
+			if isLower(result[i]) {
+				result[i] -= 32
+			}
+		} else if isUpper(result[i]) {
 			result[i] += 32
 		}
 	}
@@ -33,11 +29,9 @@ func ReverseStrCap(s string) string {
 }
 
 func main() {
-
 	if len(os.Args) > 1 {
 		for _, arg := range os.Args[1:] {
-			result := ReverseStrCap(arg)
-
+			result := reverStrCap(arg)
 			for _, r := range result {
 				z01.PrintRune(r)
 			}
