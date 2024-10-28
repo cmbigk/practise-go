6cbbe2886393cb49444bb80d1c393ae4f130b35c
chmyint <bigk89865@gmail.com>
2024-10-28 12:36:20 +0200

    okk

--- printrevcomb/printrevcomb.go
+++ printrevcomb/printrevcomb.go
@@ -7,18 +7,16 @@ func main() {
 	for i := '9'; i >= '0'; i-- {
 		for j := i - 1; j >= '0'; j-- {
 			for k := j - 1; k >= '0'; k-- {
-				// Add ", " before each combination except the first
 				if !first {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				} else {
 					first = false
 				}
-
-				// Print the current combination
 				z01.PrintRune(i)
 				z01.PrintRune(j)
 				z01.PrintRune(k)
+
 			}
 		}
 	}
