fe6387d975ee1200ea7d3c00acbc5cd851b3c5bb
cmbigk <bigk89865@gmail.com>
2024-10-28 10:03:11 +0200

    okkk

--- hiddenp/main.go
+++ hiddenp/main.go
@@ -6,6 +6,17 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func isInStr(s1, s2 string) bool {
+	i, j := 0, 0
+	for i < len(s1) && j < len(s2) {
+		if s1[i] == s2[j] {
+			i++
+		}
+		j++
+	}
+	return i == len(s1)
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		return
@@ -14,15 +25,7 @@ func main() {
 	s1 := os.Args[1]
 	s2 := os.Args[2]
 
-	i, j := 0, 0
-	for i < len(s1) && j < len(s2) {
-		if s1[i] == s2[j] {
-			i++
-		}
-		j++
-	}
-
-	if i == len(s1) {
+	if isInStr(s1, s2) {
 		z01.PrintRune('1')
 	} else {
 		z01.PrintRune('0')
