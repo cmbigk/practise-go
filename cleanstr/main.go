ea1058d8d874d3d17432d041b405643992794e0e
cmbigk <bigk89865@gmail.com>
2024-11-24 16:17:45 +0200

    edited the issue

--- cleanstr/main.go
+++ cleanstr/main.go
@@ -15,16 +15,24 @@ func main() {
 	inWord := false
 	for i, char := range str {
 		if char == ' ' || char == '\t' {
-			if inWord && i != 0 && str[i-1] != ' ' && str[i-1] != '\t' {
+			if inWord && i != 0 && str[i-1] != ' ' && str[i-1] != '\t' && !isTrailingSpaces(str, i) {
 				z01.PrintRune(' ')
-
 			}
 		} else {
 			z01.PrintRune(char)
 			inWord = true
 		}
-
 	}
-	z01.PrintRune('\n')
 
+	z01.PrintRune('$')
+	z01.PrintRune('\n')
+}
+
+func isTrailingSpaces(str string, index int) bool {
+	for i := index; i < len(str); i++ {
+		if str[i] != ' ' && str[i] != '\t' {
+			return false
+		}
+	}
+	return true
 }
